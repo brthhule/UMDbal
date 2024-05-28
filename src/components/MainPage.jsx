b44f0a257c9a6a42dcd346762d9c8713f956b26c
Brennen Le <107582405+brthhule@users.noreply.github.com>
2024-05-28 13:19:29 -0400

    Update MainPage.jsx

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -2,33 +2,38 @@ import campus from '../assets/UMD-campus.jpg'
 import '../styles/MainPage.css'
 
 const MainPage = () => {
-    var woods = {"name": "Woods", "location": 1,}
-    var maryMount = {"name": "Mary Mount", "location": 10,}
-    var dorchester = {"name": "Dorchester", "location": 100,}
-    var pyonChen = {"name": "Pyon Chen", "location": 50,};
-    //building variables
-    var buildings = [woods, maryMount, dorchester];
+    var woods = {"name": "Woods", "xCoord": 38.98520825094225, "yCoord": -76.94178927335166}
+    var maryMount = {"name": "Mary Mount", "xCoord": 38.9851333833028, "yCoord": -76.94086354821499}
+    var dorchester = {"name": "Dorchester", "xCoord": 38.98684874201056, "yCoord": -76.9461386886941}
+    var pyonChen = {"name": "Pyon Chen", "xCoord": 38.99217621364386, "yCoord": -76.94490983773476};
+    //all building variables defined
+    var buildings = [woods, maryMount, dorchester, pyonChen];
     //array of all buildings
     var list = [];
-    //closest list
-    var guess = pyonChen;
-
+    //list for guesses, ordered closest to furthests
+    var answer = pyonChen;
+    var guessed = false;
+    //answer building variable stored and if guessed variable stored
+    
     document.getElementById('userForm').addEventListener('submit', function(event) {
-        //while (guessed != true) {
-            event.preventDefault();
-            const input = document.getElementById('name').value;
-            addListItem(input);
-            displayOutput();
-            console.log("here");
-            //Testing purposes
-        //}
+        event.preventDefault();
+        const input = document.getElementById('name').value;
+        addListItem(input);
+        displayOutput();
     });
-
+    
     function addListItem(text) {
-        if (text == guess.name) {
-            console.log("correct");
-            endGame();
-        } else {
+        if (text == answer.name) {
+            guessed = true;
+            return;
+        }
+        let inList = false;
+        for (var i = 0; i < list.length; i++) {
+            if (list[i].name == text) {
+                inList = true;
+            }
+        }
+        if (inList == false) {
             for (var i = 0; i < buildings.length; i++) {
                 if (text == buildings[i].name) {
                     list.push(buildings[i]);
@@ -36,23 +41,37 @@ const MainPage = () => {
             }
         }
     }
-
+    
+    function calculateDistance(coord1, coord2) {
+        const R = 6371e3; 
+        const φ1 = coord1.xCoord * Math.PI/180;
+        const φ2 = coord2.xCoord * Math.PI/180;
+        const Δφ = (coord2.xCoord - coord1.xCoord) * Math.PI/180;
+        const Δλ = (coord2.yCoord - coord1.yCoord) * Math.PI/180;
+    
+        const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+                  Math.cos(φ1) * Math.cos(φ2) *
+                  Math.sin(Δλ/2) * Math.sin(Δλ/2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    
+        return R * c; 
+    }
+    
     function displayOutput() {
-        document.getElementById('output').textContent = `Closests:`;
-            list.sort((a, b) => a.location - b.location);
+        if (guessed == true) {
+            console.log("end game");
+            document.getElementById('output').textContent = `You Won!`;
+        } else {
+            document.getElementById('output').textContent = `Closests:`;
+            list.sort((a, b) => calculateDistance(a, answer) - calculateDistance(b, answer));
             for (var i = 0; i < list.length; i++) {
                 if (i != 0) {
                     document.getElementById('output').textContent += ',';
                 }
                 document.getElementById('output').textContent += ' ' + list[i].name;
-            }  
+            };  
+        }  
     }
-
-    function endGame() {
-        console.log("end game");
-        document.getElementById('output').textContent = `You Won!`;
-    }
-    //not working!! Start here
 
     return (
         <>
