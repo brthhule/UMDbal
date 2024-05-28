204f105b53f34cd6c4a371d4e1de01714c5cff49
brthhule <brennenle@yahoo.com>
2024-05-28 13:02:56 -0400

    Got Router to work

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+ //About.js
+ import Navbar from './NavBar'
+
+ const About = () => {
+   return (
+     <div>
+         <Navbar />
+         <h1>About</h1>
+     </div>
+   )
+ }
+
+ export default About
